cab3f5794a64712e5389ad9d4d6a9f00cb8f3d56
Luis Amez <luis.amez.vitoria@gmail.com>
2017-06-18 22:32:23 +0200

    getTotalPrice working

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -3,15 +3,27 @@ function deleteItem(e){
 }
 
 function getPriceByProduct(itemNode){
-
+  var price = itemNode.querySelector(".unit-cost .money").innerHTML; //
+  return price;
 }
 
 function updatePriceByProduct(productPrice, index){
-
+  var item = document.getElementsByClassName('item')[index];
+  var quantity = item.querySelector(".units-to-buy .quantity").value; //
+  var total = productPrice * quantity;
+  item.querySelector(".total-price .money").innerHTML = total;
+  return total;
 }
 
 function getTotalPrice() {
-
+  var items = document.getElementsByClassName('item');
+  var total = 0;
+  for (var i = 0; i < items.length; i++) {
+    var item = items[i];
+    var price = getPriceByProduct(item);
+    total += updatePriceByProduct(price, i);
+  }
+  document.querySelector(".total-final .money").innerHTML = total;
 }
 
 function createQuantityInput(){
